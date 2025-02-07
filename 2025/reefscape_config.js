011992c030e785d4b9b3aaac1115ac0e2f4c7ced
Pace-Fischer <pfischer002@stpaul.k12.mn.us>
2025-02-07 13:35:50 -0600

    hopeful starting positions

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -61,7 +61,7 @@ var config_data = `
       "filename": "2025/half_field.png",
       "clickRestriction": "one",
       "dimensions": "6 6",
-      "allowableResponses": "1 7 13 19 25 31",
+      "allowableResponses": "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36",
       "shape": "circle 5 black red true"
     }
   ],
